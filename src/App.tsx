c0e45253890fae164e1cc6574db1a58c9bd93c38
Mittensdlara <pooraghaeedelara@gmail.com>
2023-01-06 17:03:24 +0330

    Initialized the Structure of Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-primaryColor font-bold text-5xl text-center'>Hello </h1>
+    </div>
+  );
+}
+
+export default App;
